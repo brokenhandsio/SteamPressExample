c91536d8f82f1c5d359dd7c98ea702a9b64b8f27
Tim <0xtimc@gmail.com>
2017-01-24 13:08:56 +0000

    Got paginator data working with the templates

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -12,6 +12,9 @@ let memory = MemorySessions()
 let sessions = SessionsMiddleware(sessions: memory)
 drop.middleware.append(sessions)
 
+// Temporary!
+(drop.view as? LeafRenderer)?.stem.cache = nil
+
 let steamPress = SteamPress(drop: drop, blogPath: "blog")
 
 drop.get { req in
