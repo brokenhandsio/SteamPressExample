15da2c4fe30ade8caea398942e04b6ae96ddc656
Tim <0xtimc@gmail.com>
2017-01-30 01:58:25 +0000

    Use context for short snippets. Use PostgreSQL due to bug in MemoryDriver not honouring limits.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     name: "SteamPressExample",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-        .Package(url: "https://github.com/brokenhandsio/SteamPress", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/brokenhandsio/SteamPress", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,10 +3,12 @@ import Fluent
 import Sessions
 import SteamPress
 import Foundation
+import VaporPostgreSQL
 
 let drop = Droplet()
-let database = Database(MemoryDriver())
-drop.database = database
+//let database = Database(MemoryDriver())
+//drop.database = database
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
 let memory = MemorySessions()
 let sessions = SessionsMiddleware(sessions: memory)
@@ -20,16 +22,13 @@ let steamPress = SteamPress(drop: drop, blogPath: "blog")
 drop.get { req in
     var posts = try BlogPost.all()
     posts.sort { $0.created > $1.created }
+    // Todo the query should be able to sort by date and get the latest 3 - will be much more efficient
     let newPosts = posts.prefix(3)
     
     var parameters: [String: Node] = [:]
     
     if newPosts.count > 0 {
-        var postsNode = [Node]()
-        for post in newPosts {
-            postsNode.append(try post.makeNodeWithExtras())
-        }
-        parameters["posts"] = try postsNode.makeNode()
+        parameters["posts"] = try newPosts.makeNode(context: BlogPostShortSnippet())
     }
     
     return try drop.view.make("index", parameters)
