7b6a504e76cca61437e63b3b256fc81a7c9b01b0
Tim <0xtimc@gmail.com>
2017-02-06 22:48:48 +0000

    Add slug URL info box to create post page

---
+++ Public/static/js/admin/createPost.js
@@ -0,0 +1,14 @@
+$('#inputTitle').on('input',function(e){
+    var slugUrl = slugify($('#inputTitle').val())
+    $('#inputSlugUrl').val(slugUrl)
+});
+
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
