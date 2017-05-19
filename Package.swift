133bfaa0d6689060ef8b862f5a873aa2a2883a9e
Tim <0xtimc@gmail.com>
2017-05-19 12:33:23 +0100

    Use latest release

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
     name: "SteamPressExample",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/brokenhandsio/SteamPress.git", Version(0,11,0, prereleaseIdentifiers: ["alpha"])),
-        .Package(url: "https://github.com/brokenhandsio/VaporSecurityHeaders.git", Version(0,4,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/leaf-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/brokenhandsio/SteamPress.git", Version(0,11,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/brokenhandsio/VaporSecurityHeaders.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
