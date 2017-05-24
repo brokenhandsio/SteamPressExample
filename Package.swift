c5139e692d64c1102ad329fef2158682615bf3af
Tim <0xtimc@gmail.com>
2017-05-24 13:45:16 +0100

    Update Package.swift so we use releases of SteamPress

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SteamPressExample",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/brokenhandsio/SteamPress.git", Version(0,11,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/brokenhandsio/SteamPress.git", majorVersion: 0),
         .Package(url: "https://github.com/brokenhandsio/VaporSecurityHeaders.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1)
     ],
