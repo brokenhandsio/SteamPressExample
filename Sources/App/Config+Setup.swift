68919305e03e6d26975eea168c61541373b7552d
Tim <0xtimc@gmail.com>
2017-08-25 12:56:10 +0100

    Ignore line length for CSP

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -18,6 +18,8 @@ extension Config {
 
     private func setupMiddleware() throws {
         let disqusName = self["disqus", "disqusName"]?.string ?? "*"
+
+        // swiftlint:disable:next line_length
         var cspConfig = "default-src 'none'; script-src 'self' https://static.brokenhands.io https://cdn.jsdelivr.net/ https://connect.facebook.net/ https://publish.twitter.com cdn.syndication.twimg.com platform.twitter.com https://platform.linkedin.com https://ajax.googleapis.com/ https://code.jquery.com/ https://cdnjs.cloudflare.com/ https://maxcdn.bootstrapcdn.com/ https://\(disqusName).disqus.com/ https://a.disquscdn.com/; style-src 'self' https://cdn.jsdelivr.net/ *.twimg.com platform.twitter.com https://maxcdn.bootstrapcdn.com/ https://a.disquscdn.com/ https://cdnjs.cloudflare.com/ajax/libs/select2/; img-src 'self' data: https://static.brokenhands.io https://www.facebook.com cdn.syndication.twimg.com syndication.twitter.com *.twimg.com platform.twitter.com https://referrer.disqus.com/ https://a.disquscdn.com/; connect-src 'self' https://links.services.disqus.com/; font-src https://maxcdn.bootstrapcdn.com/; child-src https://disqus.com/ syndication.twitter.com platform.twitter.com www.facebook.com staticxx.facebook.com; form-action 'self'; base-uri 'self'; require-sri-for script style;"
 
         if let reportUri = self["csp", "report-uri"]?.string {
