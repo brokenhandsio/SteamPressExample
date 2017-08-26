aa6d0dc9ad3523af17d9b85e005edc8667e61510
Tim <0xtimc@gmail.com>
2017-08-27 00:15:53 +0100

    Linter fixes

--- Tests/AppTests/SteamPressExampleTests.swift
+++ Tests/AppTests/SteamPressExampleTests.swift
@@ -3,27 +3,27 @@ import App
 import Vapor
 
 class SteamPressExampleTests: XCTestCase {
-    
+
     // MARK: - All Tests
     static var allTests = [
         ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests),
         ("testSecurityHeadersSetupCorrectly", testSecurityHeadersSetupCorrectly),
         ("testThatAboutPageRouteAdded", testThatAboutPageRouteAdded),
-        ("testThatSteamPressSetUp", testThatSteamPressSetUp),
+        ("testThatSteamPressSetUp", testThatSteamPressSetUp)
     ]
-    
+
     // MARK: - Properties
     var drop: Droplet!
-    
+
     // MARK: - Overrides
     override func setUp() {
         let config = try! Config()
         try! config.setup()
-        
+
         drop = try! Droplet(config)
         try! drop.setup()
     }
-    
+
     // MARK: - Tests
     func testLinuxTestSuiteIncludesAllTests() {
         #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
@@ -35,26 +35,26 @@ class SteamPressExampleTests: XCTestCase {
                            "\(darwinCount - linuxCount) tests are missing from allTests")
         #endif
     }
-    
+
     func testSecurityHeadersSetupCorrectly() throws {
         let request = Request(method: .get, uri: "/")
         let response = try drop.respond(to: request)
-        
+
         XCTAssertEqual(response.status, .ok)
         XCTAssertEqual(response.headers["X-Frame-Options"], "DENY")
     }
-    
+
     func testThatAboutPageRouteAdded() throws {
         let request = Request(method: .get, uri: "/about/")
         let response = try drop.respond(to: request)
-        
+
         XCTAssertEqual(response.status, .ok)
     }
-    
+
     func testThatSteamPressSetUp() throws {
         let request = Request(method: .get, uri: "/blog/")
         let response = try drop.respond(to: request)
-        
+
         XCTAssertEqual(response.status, .ok)
     }
 }
