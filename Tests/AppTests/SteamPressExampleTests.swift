fae97cc852b9e00bc84f31005a3c2c0b996e9b14
Tim <0xtimc@gmail.com>
2017-08-27 00:08:07 +0100

    Add some basic tests

--- Tests/AppTests/SteamPressExampleTests.swift
+++ Tests/AppTests/SteamPressExampleTests.swift
@@ -1,16 +1,28 @@
 import XCTest
 import App
+import Vapor
 
 class SteamPressExampleTests: XCTestCase {
     
     // MARK: - All Tests
     static var allTests = [
         ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests),
+        ("testSecurityHeadersSetupCorrectly", testSecurityHeadersSetupCorrectly),
+        ("testThatAboutPageRouteAdded", testThatAboutPageRouteAdded),
+        ("testThatSteamPressSetUp", testThatSteamPressSetUp),
     ]
     
     // MARK: - Properties
+    var drop: Droplet!
     
     // MARK: - Overrides
+    override func setUp() {
+        let config = try! Config()
+        try! config.setup()
+        
+        drop = try! Droplet(config)
+        try! drop.setup()
+    }
     
     // MARK: - Tests
     func testLinuxTestSuiteIncludesAllTests() {
@@ -22,5 +34,27 @@ class SteamPressExampleTests: XCTestCase {
             XCTAssertEqual(linuxCount, darwinCount,
                            "\(darwinCount - linuxCount) tests are missing from allTests")
         #endif
+    }
+    
+    func testSecurityHeadersSetupCorrectly() throws {
+        let request = Request(method: .get, uri: "/")
+        let response = try drop.respond(to: request)
+        
+        XCTAssertEqual(response.status, .ok)
+        XCTAssertEqual(response.headers["X-Frame-Options"], "DENY")
+    }
+    
+    func testThatAboutPageRouteAdded() throws {
+        let request = Request(method: .get, uri: "/about/")
+        let response = try drop.respond(to: request)
+        
+        XCTAssertEqual(response.status, .ok)
+    }
+    
+    func testThatSteamPressSetUp() throws {
+        let request = Request(method: .get, uri: "/blog/")
+        let response = try drop.respond(to: request)
+        
+        XCTAssertEqual(response.status, .ok)
     }
 }
