e63ae46881473c89f9fe66ffaa80b4c93513db0d
Tim <0xtimc@gmail.com>
2017-08-27 13:18:02 +0100

    Fix disqus spelling mistake

--- Public/static/js/disqus.js
+++ Public/static/js/disqus.js
@@ -8,7 +8,7 @@ var disqus_config = function () {
 
 (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    var disqusName = $("#blog-post-data").data("discussName");
+    var disqusName = $("#blog-post-data").data("disqusName");
     s.src = 'https://' + disqusName + '.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
