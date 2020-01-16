0fc1c5d48f64f44bc8eaf48f74d4eb55fcc91d6c
Tim <0xtimc@gmail.com>
2020-01-16 22:55:58 +0800

    Fix all top level templates

---
+++ Sources/App/Models/PostWithShortSnippet.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SteamPress
+
+struct PostWithShortSnippet: Codable {
+    var blogID: Int?
+    var title: String
+    var contents: String
+    var author: Int
+    var created: Date
+    var lastEdited: Date?
+    var slugUrl: String
+    var published: Bool
+    var shortSnippet: String
+}
+
+extension BlogPost {
+    func  toPostWithShortSnippet() -> PostWithShortSnippet {
+        return PostWithShortSnippet(blogID: self.blogID, title: self.title, contents: self.contents, author: self.author, created: self.created, lastEdited: self.lastEdited, slugUrl: self.slugUrl, published: self.published, shortSnippet: self.shortSnippet())
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,18 +4,23 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    router.get { req -> Future<View> in
-        let posts = BlogPost.query(on: req).filter(\.published == true).sort(\.created, .descending).range(0..<3).all()
-        let disqusName = Environment.get("BLOG_DISQUS_NAME")
-        let siteTwitterHandle = Environment.get("BLOG_SITE_TWITTER_HANDLE")
-        let googleAnalyticsIdentifier = Environment.get("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
-        let loggedInUser = try req.authenticated(BlogUser.self)
-        let webpageURL = req.http.url.absoluteString
-        let context = HomepageContext(posts: posts, user: loggedInUser, twitterHandler: siteTwitterHandle, googleAnalytics: googleAnalyticsIdentifier, disqusName: disqusName, webpageURL: webpageURL)
-        return try req.view().render("index", context)
+    
+    let authSessions = router.grouped(BlogAuthSessionsMiddleware())
+    
+    authSessions.get { req -> Future<View> in
+        return BlogPost.query(on: req).filter(\.published == true).sort(\.created, .descending).range(0..<3).all().flatMap { posts in
+            let disqusName = Environment.get("BLOG_DISQUS_NAME")
+            let siteTwitterHandle = Environment.get("BLOG_SITE_TWITTER_HANDLE")
+            let googleAnalyticsIdentifier = Environment.get("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
+            let loggedInUser = try req.authenticated(BlogUser.self)
+            let webpageURL = req.http.url.absoluteString
+            let postsWithShortSnippets = posts.map { $0.toPostWithShortSnippet() }
+            let context = HomepageContext(posts: postsWithShortSnippets, user: loggedInUser, twitterHandler: siteTwitterHandle, googleAnalytics: googleAnalyticsIdentifier, disqusName: disqusName, webpageURL: webpageURL)
+            return try req.view().render("index", context)
+        }
     }
     
-    router.get("about") { req -> Future<View> in
+    authSessions.get("about") { req -> Future<View> in
         let disqusName = Environment.get("BLOG_DISQUS_NAME")
         let siteTwitterHandle = Environment.get("BLOG_SITE_TWITTER_HANDLE")
         let googleAnalyticsIdentifier = Environment.get("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
@@ -27,7 +32,7 @@ public func routes(_ router: Router) throws {
 }
 
 struct HomepageContext: Encodable {
-    let posts: Future<[BlogPost]>
+    let posts: [PostWithShortSnippet]
     let user: BlogUser?
     let twitterHandler: String?
     let googleAnalytics: String?
