6bbe4843ac64f13476decee3646a1f67de718453
Tim <0xtimc@gmail.com>
2020-01-18 10:16:37 +0800

    Make the page information the same across blog and homepage so we don't show logged in status when not logged in on about and homepage

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,42 +9,45 @@ public func routes(_ router: Router) throws {
     
     authSessions.get { req -> Future<View> in
         return BlogPost.query(on: req).filter(\.published == true).sort(\.created, .descending).range(0..<3).all().flatMap { posts in
-            let disqusName = Environment.get("BLOG_DISQUS_NAME")
-            let siteTwitterHandle = Environment.get("BLOG_SITE_TWITTER_HANDLE")
-            let googleAnalyticsIdentifier = Environment.get("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
-            let loggedInUser = try req.authenticated(BlogUser.self)
-            let webpageURL = req.http.url.absoluteString
+            let pageInformation = try req.getPageInformation()
             let postsWithShortSnippets = posts.map { $0.toPostWithShortSnippet() }
-            let context = HomepageContext(posts: postsWithShortSnippets, user: loggedInUser, twitterHandler: siteTwitterHandle, googleAnalytics: googleAnalyticsIdentifier, disqusName: disqusName, webpageURL: webpageURL)
+            let context = HomepageContext(posts: postsWithShortSnippets, pageInformation: pageInformation)
             return try req.view().render("index", context)
         }
     }
     
     authSessions.get("about") { req -> Future<View> in
-        let disqusName = Environment.get("BLOG_DISQUS_NAME")
-        let siteTwitterHandle = Environment.get("BLOG_SITE_TWITTER_HANDLE")
-        let googleAnalyticsIdentifier = Environment.get("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
-        let loggedInUser = try req.authenticated(BlogUser.self)
-        let webpageURL = req.http.url.absoluteString
-        let context = AboutPageContext(webpageURL: webpageURL, user: loggedInUser, twitterHandler: siteTwitterHandle, googleAnalytics: googleAnalyticsIdentifier, disqusName: disqusName)
+        let pageInformation = try req.getPageInformation()
+        let context = AboutPageContext(pageInformation: pageInformation)
         return try req.view().render("about", context)
     }
 }
 
 struct HomepageContext: Encodable {
     let posts: [PostWithShortSnippet]
-    let user: BlogUser?
+    let pageInformation: PageInformation
+}
+
+struct AboutPageContext: Encodable {
+    let aboutPage = true
+    let pageInformation: PageInformation
+}
+
+struct PageInformation: Encodable {
+    let loggedInUser: BlogUser?
     let twitterHandler: String?
     let googleAnalytics: String?
     let disqusName: String?
     let webpageURL: String
 }
 
-struct AboutPageContext: Encodable {
-    let aboutPage = true
-    let webpageURL: String
-    let user: BlogUser?
-    let twitterHandler: String?
-    let googleAnalytics: String?
-    let disqusName: String?
+extension Request {
+    func getPageInformation() throws -> PageInformation {
+        let disqusName = Environment.get("BLOG_DISQUS_NAME")
+        let siteTwitterHandle = Environment.get("BLOG_SITE_TWITTER_HANDLE")
+        let googleAnalyticsIdentifier = Environment.get("BLOG_GOOGLE_ANALYTICS_IDENTIFIER")
+        let loggedInUser = try self.authenticated(BlogUser.self)
+        let webpageURL = self.http.url.absoluteString
+        return PageInformation(loggedInUser: loggedInUser, twitterHandler: siteTwitterHandle, googleAnalytics: googleAnalyticsIdentifier, disqusName: disqusName, webpageURL: webpageURL)
+    }
 }
